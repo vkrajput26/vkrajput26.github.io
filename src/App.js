bbe9a2d61711ab5d34bb2028832f2bfbaeb363e7
Vivek Rajput <indvivekrajput@gmail.com>
2022-09-10 22:56:09 +0530

    Adding Media Queries

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hello world
+        <div className="App1">
+          1fffffffffffffffffffffffffffffffffff
+        </div>
+        <div className="App2">
+          2fffdddsssssssssssssssssssssssssssssssssssssssssssss
+        </div>
+        <div className="App3">
+          3hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </div>
+     
     </div>
   );
 }
